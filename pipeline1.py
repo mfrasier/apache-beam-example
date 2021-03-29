f126c7ce4a5b2b17e39db8f6b89681037468d3cc
Marty Frasier <lmf4u@virginia.edu>
2021-03-28 20:07:34 -0400

    added windowing timestamps to elements but not aggregated yet.'

--- pipeline1.py
+++ pipeline1.py
@@ -1,9 +1,27 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 import logging
+import time
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 logger = logging.getLogger("pipeline1")
+
+
+class AddTimestampDoFn(beam.DoFn):
+    """ Add timestamp to PCollection. """
+
+    def process(self, element):
+        # extract event time from element.registration_dttm field
+        event_time = extract_event_time(element)
+        # wrap and emit current element and new timestamp an a TimestampedValue
+        yield beam.window.TimestampedValue(element, event_time)
+
+
+def extract_event_time(element):
+    """ Extract event time from element. """
+    time_tuple = element['registration_dttm'].to_pydatetime().timetuple()
+    unix_time = time.mktime(time_tuple)
+    return unix_time
 
 
 def run(pipeline_options, file_pattern):
@@ -11,10 +29,11 @@ def run(pipeline_options, file_pattern):
     with beam.Pipeline(options=pipeline_options) as p:
         user_data = (
                 p
-                | beam.io.ReadFromParquet(file_pattern))
-        logger.info("retrieved data")
-        # user_data |
-        # print(f"retrieved {user_data.count()} records from {file_pattern}")
+                | 'read' >> beam.io.ReadFromParquet(file_pattern)
+                | 'timestamp' >> beam.ParDo(AddTimestampDoFn())
+                | 'window' >> beam.WindowInto(beam.window.FixedWindows(60 * 60))
+                # | 'count elements' >> beam.combiners.Count.PerKey()
+                | 'print' >> beam.Map(print))
 
 
 if __name__ == "__main__":
